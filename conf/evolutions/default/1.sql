86e1c7175ddaa6b9459ce1e14d07195545a4355e
TTCAD\r-takahashi <r-takahashi@ttc-net.co.jp>
2017-05-19 18:32:48 +0900

    2017/05/19

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user_information (
+  id                        bigint auto_increment not null,
+  user_id                   varchar(255),
+  user_name                 varchar(255),
+  password                  varchar(255),
+  constraint pk_user_information primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table user_information;
+
+SET FOREIGN_KEY_CHECKS=1;
+
